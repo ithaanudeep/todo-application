11c0275e8d8557b96a963bb061888a1b60d3e579
ithaanudeep <52530163+ithaanudeep@users.noreply.github.com>
2021-03-23 20:13:49 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { AppRegistry } from "react-native";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import 'react-native-gesture-handler';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import AppReducer from "./src/reducers";
+import { AppNavigator, middleware } from "./src/navigations/AppNavigation";
+
+const store = createStore(AppReducer, applyMiddleware(middleware));
+
+console.disableYellowBox = true;
+
+class StarterApp extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <AppNavigator />
+      </Provider>
+    );
+  }
+}
+
+AppRegistry.registerComponent("StarterApp", () => StarterApp);
+
+export default StarterApp;
